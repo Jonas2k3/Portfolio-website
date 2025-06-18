024ed74f9af6cb1b7f70b9573b0cffd53a780c2a
Jonas2k3 <jonaskoens2003@gmail.com>
2025-06-18 23:30:17 +0200

    Add icon
    Add icon and change my age

--- script/main.js
+++ script/main.js
@@ -42,18 +42,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
 function translateToGerman() {
     document.getElementById("projects-text-index").innerHTML = "Projekte";
-    document.getElementById("intro-text").innerHTML = "Mein Name ist Jonas Koens, ich bin ein 20-jähriger Student der Softwaretechnik an der Fontys University of Applied Sciences. Ich bin ein begeisterter Problemlöser, daher begeistert mich jede komplexe Programmieraufgabe. <br> <br> <a href='/pages/about.html' id='about-page-link'>Mehr über mich <img src='/images/scroll-btn.svg' id='rotated-scroll-btn'></a>";
+    document.getElementById("intro-text").innerHTML = "Mein Name ist Jonas Koens, ich bin ein 21-jähriger Software engineering Student an der Fontys University of Applied Sciences. Ich bin ein begeisterter Problemlöser, daher begeistert mich jede komplexe Programmieraufgabe. <br> <br> <a href='/pages/about.html' id='about-page-link'>Mehr über mich <img src='/images/scroll-btn.svg' id='rotated-scroll-btn'></a>";
     document.getElementById("project-text-header-index").innerHTML = "Projekte";
+    console.log(test);
 }
 
 function translateToDutch() {
     document.getElementById("projects-text-index").innerHTML = "Projecten";
-    document.getElementById("intro-text").innerHTML = "Mijn naam is Jonas Koens, ik ben een 20 jaar oude software engineering student aan de Fontys Hogeschool. Ik ben een fervent probleemoplosser, dus voor elke complexe programmeertaak wordt ik enthousiast. <br> <br> <a href='/pages/about.html' id='about-page-link'>Meer over mij <img src='/images/scroll-btn.svg' id='rotated-scroll-btn'></a>";
+    document.getElementById("intro-text").innerHTML = "Mijn naam is Jonas Koens, ik ben een 21 jaar oude software engineering student aan de Fontys Hogeschool. Ik ben een fervent probleemoplosser, dus voor elke complexe programmeertaak wordt ik enthousiast. <br> <br> <a href='/pages/about.html' id='about-page-link'>Meer over mij <img src='/images/scroll-btn.svg' id='rotated-scroll-btn'></a>";
     document.getElementById("project-text-header-index").innerHTML = "Projecten";
 }
 
 function translateToEnglish() {
     document.getElementById("projects-text-index").innerHTML = "Projects";
-    document.getElementById("intro-text").innerHTML = "My name is Jonas Koens, I am a 20-year-old software engineering student at Fontys University of Applied Sciences. I am an avid problem solver, so any complex programming task excites me. <br> <br> <a href='/pages/about.html' id='about-page-link'>More about me <img src='/images/scroll-btn.svg' id='rotated-scroll-btn'></a>";
+    document.getElementById("intro-text").innerHTML = "My name is Jonas Koens, I am a 21-year-old software engineering student at Fontys University of Applied Sciences. I am an avid problem solver, so any complex programming task excites me. <br> <br> <a href='/pages/about.html' id='about-page-link'>More about me <img src='/images/scroll-btn.svg' id='rotated-scroll-btn'></a>";
     document.getElementById("project-text-header-index").innerHTML = "Projects";
 }
